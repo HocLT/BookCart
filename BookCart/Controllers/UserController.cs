2b4b7b5adb1042c5561fee019d862581bfd25e5c
Hoàng Đức Quang <hdquang6@gmail.com>
2025-03-11 06:57:23 +0700

    Initial commit

---
+++ BookCart/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using BookCart.Data;
+using BookCart.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookCart.Controllers
+{
+    public class UserController : Controller
+    {
+        readonly BookCartDbContext _ctx;
+
+        public UserController(BookCartDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _ctx
+                .Users
+                .Include(u => u.Role)
+                .ToListAsync();
+            return View(users);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var roles = await _ctx.Roles.ToListAsync();
+            ViewBag.Roles = roles;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _ctx.Users.Add(user);
+                await _ctx.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+    }
+}
